f98b98a8d8dd1f3c0d27ebf3108d4cca951bfc92
Olczak_Mikolaj <01159104@pw.edu.pl>
2025-10-11 00:48:18 +0200

    solution 3701

---
+++ 3701.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def alternatingSum(self, nums: List[int]) -> int:
+        ans = 0
+        for i, num in enumerate(nums):
+            if i % 2 == 0:
+                ans+=num
+            else:
+                ans-=num
+        return ans
